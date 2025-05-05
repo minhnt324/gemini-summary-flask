43b2219a337856bfa1b3b28ce7e700348930acb4
minhnt324 <149750025+minhnt324@users.noreply.github.com>
2025-05-05 14:10:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -85,4 +85,4 @@ def summarize_article(article_url):
 # Run khi local hoặc deploy
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)
